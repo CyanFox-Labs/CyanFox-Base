1a60d6ec88ff16bf01ba2271934f498e8f3cbf8b
Lenny P. | RealZone22 <lenny@petschl.org>
2023-11-02 21:53:43 +0100

    Changed footer page render check to APP_ENV Added docker-compose.test.yml Added .env.testing Added test .gitlab-ci.yml (I know it will not work...)

--- resources/views/components/navigation/footer.blade.php
+++ resources/views/components/navigation/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="p-4 bg-base-200 mt-auto z-10">
     <aside class="flex justify-center">
-        <p>{{ __('messages.page') }} @if(!env('APP_DEBUG'))
+        <p>{{ __('messages.page') }} @if(!env('APP_ENV') == 'testing')
                 {{ round((microtime(true) - LARAVEL_START) * 1000, 2) }}ms
             @else
                 0ms
