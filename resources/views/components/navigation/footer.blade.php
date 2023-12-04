33ed5d8dea0a9f03d89c4cee8f5fdadab8e0fba4
Lenny P. | RealZone22 <lenny@petschl.org>
2023-12-04 18:27:19 +0100

    Added API Docs Fixed footer.blade.php

--- resources/views/components/navigation/footer.blade.php
+++ resources/views/components/navigation/footer.blade.php
@@ -1,16 +1,19 @@
-<footer class="p-4 bg-base-200 mt-auto z-10 sm:block hidden">
-    <aside>
-        <div class="flex justify-center">
-            <p>{{ __('messages.page') }} @if(env('APP_ENV') !== 'testing')
-                    {{ round((microtime(true) - LARAVEL_START) * 1000, 2) }}ms
-                @else
-                    0ms
-                @endif </p>
-            <p>&nbsp; | &nbsp;</p>
-            <p>{{ __('messages.version') }} {{ \App\Http\Controllers\VersionController::getCurrentProjectVersion() }}</p>
-        </div>
-        <div class="flex items-end justify-end text-end">
-            <p class="text-sm text-gray-500">{!! __('messages.made_with_love') !!}</p>
-        </div>
-    </aside>
+<footer class="w-full bg-base-200 p-4 flex items-center justify-between footer mt-auto z-10">
+    <div class="flex-grow"></div>
+
+    <div class="absolute left-0 right-0 mx-auto text-center text-sm" style="width: fit-content;">
+        {{ __('messages.page') }} @if(env('APP_ENV') !== 'testing')
+                {{ round((microtime(true) - LARAVEL_START) * 1000, 2) }}ms
+            @else
+                0ms
+            @endif
+        &nbsp; | &nbsp;
+        {{ __('messages.version') }} {{ \App\Http\Controllers\VersionController::getCurrentProjectVersion() }}
+    </div>
+
+    <div class="flex-grow"></div>
+
+    <div class="text-right pr-5 text-sm">
+        <p class="text-sm">{!! __('messages.made_with_love') !!}</p>
+    </div>
 </footer>
