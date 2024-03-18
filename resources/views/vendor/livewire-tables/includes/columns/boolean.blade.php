275b36c915d5203d2c18db3412e7cefcfb2547fe
RealZone22 | Lenny P. <lenny@petschl.org>
2024-03-18 18:02:33 +0100

    Fixed icons

--- resources/views/vendor/livewire-tables/includes/columns/boolean.blade.php
+++ resources/views/vendor/livewire-tables/includes/columns/boolean.blade.php
@@ -2,9 +2,9 @@
     @if ($status)
         @if ($type === 'icons')
             @if ($successValue === true)
-                <i class="icon-check-circle-2 text-xl text-green-500"></i>
+                <i class="icon-circle-check text-xl text-green-500"></i>
             @else
-                <i class="icon-check-circle-2 text-xl text-red-500"></i>
+                <i class="icon-circle-check text-xl text-red-500"></i>
             @endif
         @elseif ($type === 'yes-no')
             @if ($successValue === true)
@@ -16,9 +16,9 @@
     @else
         @if ($type === 'icons')
             @if ($successValue === false)
-                <i class="icon-x-circle text-xl text-green-500"></i>
+                <i class="icon-circle-x text-xl text-green-500"></i>
             @else
-                <i class="icon-x-circle text-xl text-red-500"></i>
+                <i class="icon-circle-x text-xl text-red-500"></i>
             @endif
         @elseif ($type === 'yes-no')
             @if ($successValue === false)
@@ -32,9 +32,9 @@
     @if ($status)
         @if ($type === 'icons')
             @if ($successValue === true)
-                <i class="icon-x-circle text-xl text-success"></i>
+                <i class="icon-circle-x text-xl text-success"></i>
             @else
-                <i class="icon-x-circle text-xl text-danger"></i>
+                <i class="icon-circle-x text-xl text-danger"></i>
             @endif
         @elseif ($type === 'yes-no')
             @if ($successValue === true)
@@ -46,9 +46,9 @@
     @else
         @if ($type === 'icons')
             @if ($successValue === false)
-                <i class="icon-x-circle text-xl text-success"></i>
+                <i class="icon-circle-x text-xl text-success"></i>
             @else
-                <i class="icon-x-circle text-xl text-danger"></i>
+                <i class="icon-circle-x text-xl text-danger"></i>
             @endif
         @elseif ($type === 'yes-no')
             @if ($successValue === false)
