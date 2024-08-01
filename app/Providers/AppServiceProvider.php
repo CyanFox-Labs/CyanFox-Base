15431ae3289e1f38bd2aa8ff060d51c2f2e4a5f0
RealZone22 <RealZone22@users.noreply.github.com>
2024-08-01 19:48:04 +0000

    Fix styling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(ViewIntegrationService::class, function () {
-            return new ViewIntegrationService();
+            return new ViewIntegrationService;
         });
     }
 
